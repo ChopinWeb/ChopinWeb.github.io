9b185c9d044f3140384a1a30c7e3b205616bb218
ChopinWeb <125790867+ChopinWeb@users.noreply.github.com>
2023-04-13 00:52:53 +0900

    footer to responsive design

--- js/header_menu.js
+++ js/header_menu.js
@@ -1,12 +1,12 @@
 $(".menubtn").click(function () { //ボタンがクリックされたら
   $(this).toggleClass('active'); //ボタン自身にactiveクラスを付与
-    $(".nav_container").toggleClass('panelactive'); //ナビゲーションにpanelactiveクラスを付与
+    $(".header_nav").toggleClass('panelactive'); //ナビゲーションにpanelactiveクラスを付与
 });
 
-$(".nav_container li").click(function () { //ナビゲーションのリンクがクリックされたら
+$(".header_nav li").click(function () { //ナビゲーションのリンクがクリックされたら
 	console.log(document.getElementsByClassName("menubtn"));
 	if (document.getElementsByClassName("menubtn").item(0).classList.contains("active")){ //ボタンがactiveのときのみ（スマホサイズのときのみ通過可能）
     	$(".menubtn").removeClass('active');　//ボタンのactiveクラスを除去
-    	$(".nav_container").removeClass('panelactive'); //ナビゲーションのpanelactiveクラスも除去
+    	$(".header_nav").removeClass('panelactive'); //ナビゲーションのpanelactiveクラスも除去
     }
 });
